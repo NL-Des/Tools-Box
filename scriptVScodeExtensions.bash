84e46ebc910d0cd9407c148b49fbbadb0b079db0
nleonard <nathan33.travail@gmail.com>
2025-11-26 15:26:28 +0100

    Scripts d'extensions pour VScode V1.

---
+++ scriptVScodeExtensions.bash
@@ -0,0 +1,25 @@
+# Installation d'extensions VSCode.
+echo "Installation des extensions VSCode."
+
+# Extensions de conforts.
+code --install-extension esbenp.prettier-vscode          # Prettier
+code --install-extension ms-vscode-remote.remote-containers # Dev Containers
+# Extensions Git.
+code --install-extension eamodio.gitlens                 # GitLens
+code --install-extension mhutchie.git-graph              # Git Graph
+
+# Extension Docker.
+code --install-extension ms-azuretools.vscode-containers # Docker
+
+# Extensions Markdown.
+code --install-extension yzhang.markdown-all-in-one      # Markdown All in One
+
+# Extension Live Server.
+code --install-extension ritwickdey.LiveServer           # Live Server
+
+# Extensions bases de données.
+code --install-extension yy0931.vscode-sqlite3-editor    # SQLite 3
+
+# Extension GO.
+code --install-extension golang.go                       # GO
+
